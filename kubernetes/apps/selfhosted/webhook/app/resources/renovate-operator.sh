ab5bb18cd0a9538dc805c2bf361e069b858350ec
Erwan Leboucher <erwanleboucher@gmail.com>
2026-03-30 19:48:14 +0200

    push the whole payload to the webhook

--- kubernetes/apps/selfhosted/webhook/app/resources/renovate-operator.sh
+++ kubernetes/apps/selfhosted/webhook/app/resources/renovate-operator.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 JOB=${1:-}
 NAMESPACE=${2:-}
 RENOVATE_OPERATOR_WEBHOOK_URL=${3:-}
+FORGEJO_EVENT=${4:-}
 
 curl -v -X POST \
   -H "Content-Type: application/json" \
+  -H "X-Forgejo-Event: ${FORGEJO_EVENT}" \
+  -d @"${WEBHOOK_PAYLOAD_FILE}" \
   "${RENOVATE_OPERATOR_WEBHOOK_URL}/webhook/v1/forgejo?job=${JOB}&namespace=${NAMESPACE}"
